4cbf999f48ed91017d6099746358018bafe62fb7
Wanseo JO <21700693@handong.edu>
2018-04-05 01:13:58 +0900

    Add the comment and commit

--- src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
@@ -1,28 +1,28 @@
-package edu.handong.csee.java.lab07.prob1;
+package edu.handong.csee.java.lab07.prob1; // This class is belong to this package
 
-public class YearToCentury {
-	int year;
+public class YearToCentury { // define the class 
+	int year; // define the instance valuable
 
-	public YearToCentury() 
+	public YearToCentury() // constructor(special method) -1 
 	{
-		year = 0;
+		year = 0; // define year to zero 
 	}
 
-	public YearToCentury(int year)
+	public YearToCentury(int year) //constructor -2 that have parameter 
 	{
-		this.year = year;
+		this.year = year; // put parameter year to instance valuable year
 	}
 
-	public int calc_century()
+	public int calc_century() // define method that have return integer 
 	{
-		if(year % 100 == 0)
-			year = year / 100;
+		if(year % 100 == 0) // if input year % 100==0  
+			year = year / 100; // calculate and put the result in year
 
-		else
+		else //else 
 		{
-			year = (year / 100) + 1;
+			year = (year / 100) + 1; //calculate and put the result in year
 		}
-		return year;
+		return year; //return the year (return the century)
 	}
 }
 
--- src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury_main.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury_main.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 
 public class YearToCentury_main { //define a class YearToCentury_main to public
 
-	public static void main(String[] args) { //start the class declare
+	public static void main(String[] args) { //start the class declare.. main method
 
 		int yr =0; // year variable declare
 
-		//Scanner is the class name keyboard is a mean for input, define new Scanner class and call object(Data.Action)
+		//Scanner is the class name and keyboard is an instance, Order new instance
 		Scanner keyboard = new Scanner(System.in); 
 		System.out.println("Input year: "); // print the line
 		yr = keyboard.nextInt();// assign integer input from keyboard to yr
 
 		YearToCentury year = new YearToCentury(yr); // need to declare the class
-		System.out.println(yr + " is " + year.calc_century() + "th century.");
+		System.out.println(yr + " is " + year.calc_century() + "th century.");//print the line
 	}
 
 }
