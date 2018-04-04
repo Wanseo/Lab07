8f0aa74caa93f30d7375537eafb4148e74e172e1
Wanseo JO <21700693@handong.edu>
2018-04-05 02:41:23 +0900

    Add lab07 prob.2

--- src/main/java/edu/handong/csee/java/labo7/prob2/Chicken_main.java
+++ src/main/java/edu/handong/csee/java/labo7/prob2/Chicken_main.java
@@ -1,7 +1,19 @@
 package edu.handong.csee.java.labo7.prob2;
 
 public class Chicken_main {
-	
-	
+	public static void main(String[] args) {
+		Chicken menu1 = new Chicken("Cheese_mustard", 16000.00, 5);
+		Chicken menu2 = new Chicken("Honey_mustard", 16000.00, 5);
+		Chicken menu3 = new Chicken("Spicey_chicken", 16000.00, 1);
+
+		menu1.set_stars(3);
+		menu2.set_stars(4);
+		menu3.set_stars(1);
+
+		System.out.println(menu1.get_name() + "'s rating is " + menu1.get_stars());
+		System.out.println(menu2.get_name() + "'s rating is " + menu2.get_stars());
+		System.out.println(menu3.get_name() + "'s rating is " + menu3.get_stars());
+	}
+
 
 }
