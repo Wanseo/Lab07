1a0fd307de6d97ad22556fc004a2ef4bf07c0dfa
Wanseo JO <21700693@handong.edu>
2018-04-05 13:30:04 +0900

    Add the package and classes lab07 prob3

---
+++ src/main/java/edu/handong/csee/java/lab07/prob3/Attendance.java
@@ -0,0 +1,39 @@
+package edu.handong.csee.java.lab07.prob3;
+
+public class Attendance {
+
+	public Attendance(String string, int i, String string2, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void set_id(String string) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void set_name(String string) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void set_year(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void set_missed(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int get_missed() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String get_name() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
