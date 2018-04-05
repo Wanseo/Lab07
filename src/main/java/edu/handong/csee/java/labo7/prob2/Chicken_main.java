71d37c345758200ce3eaa48d3fe699876fc7ed78
Wanseo JO <21700693@handong.edu>
2018-04-05 21:59:42 +0900

    Add the comments

--- src/main/java/edu/handong/csee/java/labo7/prob2/Chicken_main.java
+++ src/main/java/edu/handong/csee/java/labo7/prob2/Chicken_main.java
@@ -1,10 +1,10 @@
-package edu.handong.csee.java.labo7.prob2;
+package edu.handong.csee.java.labo7.prob2; // define the package name
 
-public class Chicken_main {
-	public static void main(String[] args) {
-		Chicken menu1 = new Chicken("Cheese_mustard", 16000.00, 5);
-		Chicken menu2 = new Chicken("Honey_mustard", 16000.00, 5);
-		Chicken menu3 = new Chicken("Spicey_chicken", 16000.00, 1);
+public class Chicken_main { // class name is Chicken_main
+	public static void main(String[] args) { //main method
+		Chicken menu1 = new Chicken("Cheese_mustard", 16000.00, 5); //create instance that name menu 1
+		Chicken menu2 = new Chicken("Honey_mustard", 16000.00, 5);//create instance that name menu 2
+		Chicken menu3 = new Chicken("Spicey_chicken", 16000.00, 1);//create instance that name menu 3
 
 		menu1.set_stars(3);
 		menu2.set_stars(4);
